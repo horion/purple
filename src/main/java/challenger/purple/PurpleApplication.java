4fe770aad98439d8fc0907f89256fe4050ce153b
Luiz Santos <luiz.felipe@usto.re>
2019-11-02 18:48:34 -0300

    Removendo o tomcat e os banners

--- src/main/java/challenger/purple/PurpleApplication.java
+++ src/main/java/challenger/purple/PurpleApplication.java
@@ -2,8 +2,9 @@ package challenger.purple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class,})
 public class PurpleApplication {
 
     public static void main(String[] args) {
